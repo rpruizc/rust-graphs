649187932b3e05954bf99f69357890f7ebb1b896
Rodolfo Ruiz <rpruiz@me.com>
2021-06-30 13:01:26 -0500

    Initial readme and graph types

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tests with graphs in rust
+# Tests with graphs in rust
+
+## A homogeneous graph
+
+Inspired by https://smallcultfollowing.com/babysteps/blog/2015/04/06/modeling-graphs-in-rust-using-vector-indices/
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// The algorithm traverses the graph and memoizes intermediate values
+// Memoization is preferred as paths grow exponentially as more nodes
+// are added, recursive implementations can be slow.
+
+#[derive(Copy, Clone)]
+pub struct Idx(usize);
+
+pub struct Node {
+    children: Vec<Idx>,
+}
+
+#[derive(Default)]
+pub struct Graph {
+    nodes: Vec<Node>,
 }
