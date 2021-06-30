eeb16cfa71fc0a3d01a9a083ae595575d6732c05
Rodolfo Ruiz <rpruiz@me.com>
2021-06-30 17:39:28 -0500

    Graphical representation of all paths for the used test case

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,11 @@ fn main() {
     let c = g.push(vec![a, b]);
     let d = g.push(vec![a, b, c]);
 
+    // All possible paths are:
+    // a -> b -> c -> d
+    // a -> b ------> d
+    // a ------> c -> d
+    // a -----------> d
+
     assert_eq!(4, g.count_paths());
 }
